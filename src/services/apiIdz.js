49b812cf279b25e530e4a878145db78a62e90c2a
Allan Stenks <allan.stenks@gmail.com>
2025-01-12 22:41:52 -0300

    Ajustes

--- src/services/apiIdz.js
+++ src/services/apiIdz.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const isProduction = false;
+const isProduction = true;
 
 const api = axios.create({
     baseURL: isProduction ? process.env.REACT_APP_API_URL || "https://api.dreamroute.com.br/" : process.env.REACT_APP_HML_URL || "https://dreamroute.idzcar.com.br/",
